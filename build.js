1ea14a481a56fe08edc8b0da99e192dc363b1216
Chris Mar <cmar@customink.com>
2021-11-30 06:56:24 -0500

    prettier files

--- build.js
+++ build.js
@@ -1,52 +1,54 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 function buildPrices() {
-  const dataDir = path.join(__dirname, '/data')
-  const fileList = fs.readdirSync(dataDir)
-  const mostRecentFileName = fileList.sort().pop()
+  const dataDir = path.join(__dirname, "/data");
+  const fileList = fs.readdirSync(dataDir);
+  const mostRecentFileName = fileList.sort().pop();
   const rawdata = fs.readFileSync(path.join(dataDir, mostRecentFileName));
-  return JSON.parse(rawdata)
+  return JSON.parse(rawdata);
 }
 
 function readOverrides() {
-  const setsFileName = './overrides.json'
+  const setsFileName = "./overrides.json";
   const setsRawdata = fs.readFileSync(setsFileName);
   return JSON.parse(setsRawdata);
 }
 
 function buildCatalog() {
-  const prices = buildPrices()
-  const catalog = {}
+  const prices = buildPrices();
+  const catalog = {};
 
-  prices.forEach((item) => {
+  prices.forEach(item => {
     catalog[item.slug] = {
       brand: item.brand.name,
       name: item.name,
       rarity: item.rarity,
       totalIssued: item.totalIssued,
-      floorPrice: item.metrics.lowestPrice
-    }
-  })
+      floorPrice: item.metrics.lowestPrice,
+    };
+  });
 
-  return catalog
+  return catalog;
 }
 
 function buildSets() {
-  const catalog = buildCatalog()
-  const setsFileName = './sets.json'
+  const catalog = buildCatalog();
+  const setsFileName = "./sets.json";
   const setsRawdata = fs.readFileSync(setsFileName);
   const sets = JSON.parse(setsRawdata);
 
-  const overrides = readOverrides()
+  const overrides = readOverrides();
 
   sets.forEach(set => {
     // replace slugs with collectibles
     set.slugs.forEach(slug => {
-      if (!set.collectibles) { set.collectibles = [] }
-      set.collectibles.push(catalog[slug])
-    })
-    delete set.slugs
+      if (!set.collectibles) {
+        set.collectibles = [];
+      }
+      set.collectibles.push(catalog[slug]);
+    });
+    delete set.slugs;
 
     // Apply Overrides
     // set.collectibles.forEach(c => {
@@ -56,26 +58,34 @@ function buildSets() {
 
     // calcuate total floor price of collectibles
     if (set.collectibles) {
-      set.total = set.collectibles.reduce((prev, current) => prev + current.floorPrice, 0)
-      set.marketCap = set.collectibles.reduce((prev, current) => prev + current.floorPrice * current.totalIssued, 0)
+      set.total = set.collectibles.reduce(
+        (prev, current) => prev + current.floorPrice,
+        0
+      );
+      set.marketCap = set.collectibles.reduce(
+        (prev, current) => prev + current.floorPrice * current.totalIssued,
+        0
+      );
     } else {
-      set.total = 0
-      set.marketCap = 0
+      set.total = 0;
+      set.marketCap = 0;
     }
 
-    console.log(set.name, set.total, set.marketCap)
+    console.log(set.name, set.total, set.marketCap);
+  });
 
-  })
-
-  return sets
+  return sets;
 }
 
 // main
-const sets = buildSets()
+const sets = buildSets();
 
-const buildDir = 'build'
+const buildDir = "build";
 if (!fs.existsSync(buildDir)) {
-  fs.mkdirSync(buildDir)
+  fs.mkdirSync(buildDir);
 }
 
-fs.writeFileSync(path.resolve(buildDir, './current.json'), JSON.stringify(sets, null, 2));
+fs.writeFileSync(
+  path.resolve(buildDir, "./current.json"),
+  JSON.stringify(sets, null, 2)
+);
