6fa4bbf895c140aff856364d0683e19b2ea5d2c1
Chris Mar <cmar@customink.com>
2021-12-27 11:59:04 -0500

    write comics export as csv

--- scripts/comics-export.js
+++ scripts/comics-export.js
@@ -3,8 +3,8 @@ const fs = require("fs");
 const path = require("path");
 
 function readComics() {
-    const rawdata = fs.readFileSync(path.join('./comics.v2.json'));
-    return JSON.parse(rawdata)
+  const rawdata = fs.readFileSync(path.join("./comics.v2.json"));
+  return JSON.parse(rawdata);
 }
 
 // let seriesId = 0;
@@ -13,40 +13,57 @@ function readComics() {
 // }
 
 function formatRarity(rarity) {
-    switch (rarity) {
-        case "COMMON":
-            return "Common";
-        case "UNCOMMON":
-            return "Uncommon";
-        case "RARE":
-            return "Rare";
-        case "ULTRA_RARE":
-            return 'Ultra Rare';
-        case "SECRET_RARE":
-            return "Secret Rare";
-    }
+  switch (rarity) {
+    case "COMMON":
+      return "Common";
+    case "UNCOMMON":
+      return "Uncommon";
+    case "RARE":
+      return "Rare";
+    case "ULTRA_RARE":
+      return "Ultra Rare";
+    case "SECRET_RARE":
+      return "Secret Rare";
+  }
 }
 
 const comics = readComics();
 const issues = [];
 
 comics.forEach(comic => {
-    comic.variants.forEach(v => {
-        issues.push({
-            id: v.id,
-            name: [comic.series, `#${comic.issue}`].join(' '),
-            rarity: v.rarity,
-            totalIssued: v.totalIssued,
-            floorPrice: v.floorPrice
-        })
-    })
-})
+  comic.variants.forEach(v => {
+    issues.push({
+      id: v.id,
+      name: [comic.series, `#${comic.issue}`].join(" "),
+      rarity: formatRarity(v.rarity),
+      totalIssued: v.totalIssued,
+      floorPrice: v.floorPrice,
+    });
+  });
+});
 
 issues.sort((a, b) => a.id - b.id);
 
-issues.forEach(i => {
-    console.log(`${i.id}, "${i.name}", "${formatRarity(i.rarity)}", ${i.totalIssued}, ${i.floorPrice}`)
-})
+const createCsvStringifier = require("csv-writer").createObjectCsvStringifier;
 
-const totalCount = issues.reduce((current, next) => current + next.totalIssued, 0);
+const csvStringifier = createCsvStringifier({
+  header: [
+    { id: "id", title: "id" },
+    { id: "name", title: "name" },
+    { id: "rarity", title: "rarity" },
+    { id: "totalIssued", title: "Total Editions" },
+    { id: "floorPrice", title: "Floor Price" },
+  ],
+});
+
+console.log(csvStringifier.stringifyRecords(issues));
+
+// issues.forEach(i => {
+//     console.log(`${i.id}, "${i.name}", "${formatRarity(i.rarity)}", ${i.totalIssued}, ${i.floorPrice}`)
+// })
+
+const totalCount = issues.reduce(
+  (current, next) => current + next.totalIssued,
+  0
+);
 console.log(totalCount);
